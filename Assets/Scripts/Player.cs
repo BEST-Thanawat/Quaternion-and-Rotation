2335f39d24bed12f5693962cad12107babb8f684
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-06-23 22:12:53 -0700

    Test rigidbody with MovePosition and MoveRotation.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,110 +2,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    [Space(3)]
-    [Header("Quaternion", order = 0)]
     [SerializeField] private GameObject enemy;
     [SerializeField] private Text distanceToEnemy;
-    public float RotationSpeed = 4f;
 
-    
-    float smoothInputMagnitude;
-    float smoothMoveVelocity;
-    float angle;
-    Vector3 velocity;
+    //New Input System
+    private InputMaster controls;
+
     Rigidbody rigidbody;
 
-    [Space(3)]
-    [Header("Movement", order = 1)]
-    public float moveSpeed = 7;
-    public float turnSpeed = 8f;
-    public float smoothMoveTime = 0.1f;
-    
+    //Movement
+    private Vector2 movementInput;
+    private Vector3 movement;
+    private float speed = 4.5f;
+    private Vector3 inputDirection;
 
+    //Turn
+    private Vector2 turnInput;
+    [SerializeField] private float MovingTurnSpeed = 360;
+    [SerializeField] private float StationaryTurnSpeed = 180;
+    [SerializeField] private float ForwardAmount = 1f;
+
+    private void Awake()
+    {
+        controls = new InputMaster();
+        controls.Player.Shoot.performed += context => Shoot();
+        controls.Player.Movement.performed += context => movementInput = context.ReadValue<Vector2>();
+        controls.Player.Turn.performed += context => turnInput = context.ReadValue<Vector2>();
+        //Vector2 pos = Camera.main.ScreenToViewportPoint(Mouse.current.position.ReadValue());
+    }
+    
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
-    private void FixedUpdate()
-    {
-        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
-        rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
-    }
+    
 
     private void Update()
     {
-        Vector3 inputDirection = Vector3.zero;
-        inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
 
-        float inputMagnitude = inputDirection.magnitude;
-        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
-
-        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
-        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
-
-        velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+    }
+    void FixedUpdate()
+    {
+        MoveThePlayer();
+        TurnThePlayer();
     }
 
-    public void Rotate90ToEnemy()
+    private void MoveThePlayer()
     {
-        StartCoroutine(Rotate90CT());
+        var targetInput = new Vector3(movementInput.x, 0, movementInput.y);
+        inputDirection = Vector3.Lerp(inputDirection, targetInput, Time.deltaTime * 10f);
+
+        movement.Set(inputDirection.x, 0f, inputDirection.z);
+        movement = movement * speed * Time.deltaTime;
+        rigidbody.MovePosition(transform.position + movement);
+
+        //rigidbody.velocity = new Vector3(desiredDirection.x, rigidbody.velocity.y, desiredDirection.y);
     }
 
-    public void DrawDistance()
-    {
-        StartCoroutine(DrawDistanceCT());
-    }
-    public void DrawDirection()
-    {
-        StartCoroutine(DrawDirectionCT());
-    }
-    IEnumerator Rotate90CT()
-    {
-        while (true)
+    void TurnThePlayer()
+    { 
+        Quaternion deltaRotation = Quaternion.identity;
+        Ray ray = Camera.main.ScreenPointToRay(turnInput);
+        
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f))
         {
-            Vector3 direction = (enemy.transform.position - transform.position).normalized;
-            float distance = (enemy.transform.position - transform.position).magnitude;
-
-            Vector3 relativePosition = (enemy.transform.position - transform.position).normalized;
-            Quaternion lookRotation = Quaternion.LookRotation(relativePosition);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, RotationSpeed * Time.deltaTime);
-            //transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, RotationSpeed * Time.deltaTime);
-            //float targetAngle = Mathf.Atan2(relativePosition.z, relativePosition.x) * Mathf.Rad2Deg;
-            //Quaternion offset = Quaternion.Euler(0, 90, 0);
-            //Quaternion targetAngle = Quaternion.Euler(relativePosition.z, 0, relativePosition.x);
-            //transform.rotation = Quaternion.Lerp(transform.rotation, targetAngle, RotationSpeed * Time.deltaTime);
-
-            
-            yield return null;
+            Vector3 move = transform.InverseTransformPoint(hit.point);
+            float m_TurnAmount = Mathf.Atan2(move.x, move.z);
+            float turnSpeed = Mathf.Lerp(StationaryTurnSpeed, MovingTurnSpeed, ForwardAmount);
+            deltaRotation = Quaternion.Euler(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
         }
+
+        //Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, 30, 0) * Time.deltaTime);
+        rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
     }
 
-    IEnumerator DrawDistanceCT()
-    {
-        while (true)
-        {
-            Vector3 direction = (enemy.transform.position - transform.position);
-            float distance = direction.magnitude;
+    private void OnEnable() => controls.Player.Enable();
+    private void OnDisable() => controls.Player.Disable();
 
-            Vector3 directionLine = transform.position + (direction.normalized * distance);
-            Debug.DrawLine(transform.position, directionLine, Color.red);
-            distanceToEnemy.text = "Distance between player and enemy (Red Line): " + distance.ToString();
-
-            yield return null;
-        }
-    }
-    IEnumerator DrawDirectionCT()
+    public void Shoot()
     {
-        while (true)
-        {
-            Vector3 direction = (enemy.transform.position - transform.position);
-            Vector3 directionLine = transform.position + (direction.normalized * 2f);
-            Debug.DrawLine(transform.position, directionLine, Color.blue);
-            yield return null;
-        }
+        Debug.Log("Shoot");
     }
 }
---
+++ Assets/Scripts/PlayerFieldOfView.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PlayerFieldOfView : MonoBehaviour
+{
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private Text distanceToEnemy;
+    public float RotationSpeed = 4f;
+    private InputMaster controls;
+
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    float angle;
+    Vector3 velocity;
+    Rigidbody rigidbody;
+
+    // Move
+    private Vector2 movementInput;
+    private Vector3 movement;
+    private float speed = 4.5f;
+    private Vector3 inputDirection;
+
+    //Turn
+    private Vector2 turnInput;
+
+    public float moveSpeed = 7;
+    //public float turnSpeed = 8f;
+    public float smoothMoveTime = 0.1f;
+
+    private void Awake()
+    {
+    }
+    
+    private void Start()
+    {
+    }
+    void FixedUpdate()
+    {
+    }
+
+    private void Update()
+    {
+        //Vector3 inputDirection = Vector3.zero;
+        //inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+
+        //float inputMagnitude = inputDirection.magnitude;
+        //smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        //float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        //angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+
+        //velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+    }
+
+    public void Shoot()
+    {
+        Debug.Log("Shoot");
+    }
+
+    public void Rotate90ToEnemy()
+    {
+        StartCoroutine(Rotate90CT());
+    }
+
+    public void DrawDistance()
+    {
+        StartCoroutine(DrawDistanceCT());
+    }
+    public void DrawDirection()
+    {
+        StartCoroutine(DrawDirectionCT());
+    }
+    IEnumerator Rotate90CT()
+    {
+        while (true)
+        {
+            Vector3 direction = (enemy.transform.position - transform.position).normalized;
+            float distance = (enemy.transform.position - transform.position).magnitude;
+
+            Vector3 relativePosition = (enemy.transform.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(relativePosition);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, RotationSpeed * Time.deltaTime);
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, RotationSpeed * Time.deltaTime);
+            //float targetAngle = Mathf.Atan2(relativePosition.z, relativePosition.x) * Mathf.Rad2Deg;
+            //Quaternion offset = Quaternion.Euler(0, 90, 0);
+            //Quaternion targetAngle = Quaternion.Euler(relativePosition.z, 0, relativePosition.x);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, targetAngle, RotationSpeed * Time.deltaTime);
+
+            
+            yield return null;
+        }
+    }
+
+    IEnumerator DrawDistanceCT()
+    {
+        while (true)
+        {
+            Vector3 direction = (enemy.transform.position - transform.position);
+            float distance = direction.magnitude;
+
+            Vector3 directionLine = transform.position + (direction.normalized * distance);
+            Debug.DrawLine(transform.position, directionLine, Color.red);
+            distanceToEnemy.text = "Distance between player and enemy (Red Line): " + distance.ToString();
+
+            yield return null;
+        }
+    }
+    IEnumerator DrawDirectionCT()
+    {
+        while (true)
+        {
+            Vector3 direction = (enemy.transform.position - transform.position);
+            Vector3 directionLine = transform.position + (direction.normalized * 2f);
+            Debug.DrawLine(transform.position, directionLine, Color.blue);
+            yield return null;
+        }
+    }
+}
