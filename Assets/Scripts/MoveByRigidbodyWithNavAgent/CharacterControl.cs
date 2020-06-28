3718d19ceec8991f57500835acbf0a1ea925f319
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-06-27 17:05:31 -0700

    Add ground detector.

--- Assets/Scripts/MoveByRigidbodyWithNavAgent/CharacterControl.cs
+++ Assets/Scripts/MoveByRigidbodyWithNavAgent/CharacterControl.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Processors;
 
 public enum TransitionParameter
 {
     Turn,
     Forward,
     Jump,
-    ForceTransition
+    ForceTransition,
+    Grounded
 }
 public class CharacterControl : MonoBehaviour
 {
-    public float Speed;
     public Animator CharacterAnimator;
-
     public bool PressedW;
     public bool PressedA;
     public bool PressedS;
@@ -24,7 +24,9 @@ public class CharacterControl : MonoBehaviour
     public Vector2 KeyboardPressedValue;
     public Vector2 RotateValue;
     public bool Jump;
+    public GameObject ColliderEdgePrefab;
 
+    public List<GameObject> BottomSpheres = new List<GameObject>();
     private Rigidbody rigid;
     public Rigidbody RIGID_BODY
     {
@@ -36,5 +38,40 @@ public class CharacterControl : MonoBehaviour
             }
             return rigid;
         }
+    }
+
+    private void Awake()
+    {
+        CapsuleCollider capsule = GetComponent<CapsuleCollider>();
+
+        float bottom = capsule.bounds.center.y - capsule.bounds.extents.y;
+        float top = capsule.bounds.center.y + capsule.bounds.extents.y;
+        float front = capsule.bounds.center.z + capsule.bounds.extents.z;
+        float back = capsule.bounds.center.z - capsule.bounds.extents.z;
+
+        GameObject bottomFront = CreateEdgeSphere(new Vector3(0f, bottom, front));
+        GameObject bottomBack = CreateEdgeSphere(new Vector3(0f, bottom, back));
+
+        bottomFront.transform.parent = this.transform;
+        bottomBack.transform.parent = this.transform;
+
+        BottomSpheres.Add(bottomFront);
+        BottomSpheres.Add(bottomBack);
+
+        float sec = (bottomFront.transform.position - bottomBack.transform.position).magnitude / 5f;
+
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 pos = bottomBack.transform.position + (Vector3.forward * sec * (i + 1));
+            GameObject newObj = CreateEdgeSphere(pos);
+            newObj.transform.parent = this.transform;
+            BottomSpheres.Add(newObj);
+        }
+    }
+
+    private GameObject CreateEdgeSphere(Vector3 position)
+    {
+        GameObject obj = Instantiate(ColliderEdgePrefab, position, Quaternion.identity);
+        return obj;
     }
 }
--- Assets/Scripts/MoveByRigidbodyWithNavAgent/StateScripts/Jump.cs
+++ Assets/Scripts/MoveByRigidbodyWithNavAgent/StateScripts/Jump.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New State", menuName = "MyGame/AbilityData/Jump")]
 public class Jump : StateData
 {
+    public float JumpForce;
     public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
     {
-
+        characterState.GetCharacterControl(animator).RIGID_BODY.AddForce(Vector3.up * JumpForce);
     }
 
-    public override void UpdateAbility(CharacterState characterStateBase, Animator animator, AnimatorStateInfo stateInfo)
+    public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
     {
 
     }
